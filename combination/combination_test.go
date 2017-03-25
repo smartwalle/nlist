22559b29831856e7a796787cf60386bc4f34ba4b
SmartWalle <smartwalle@gmail.com>
2017-03-25 12:54:29 +0800

    Combination

--- combination/combination_test.go
+++ combination/combination_test.go
@@ -8,10 +8,11 @@ import (
 func TestCombination(t *testing.T) {
 	//var l1 = []interface{}{"A", "K", "Q", "J", "10", "9", "8", "7", "6", "5", "4", "3", "2"}
 	//var l2 = []interface{}{"♠", "♥", "♦", "♣"}
-	var l1 = []interface{}{1, 2, 3}
+	var l1 = []interface{}{1, 2, 3, 4}
 	var l2 = []interface{}{"A", "B", "C", "D"}
+	var l3 = []interface{}{"a", "b"}
 
-	var p = [][]interface{}{l1, l2}
+	var p = [][]interface{}{l1, l2, l3}
 
 	fmt.Println(Combination(p))
 }
